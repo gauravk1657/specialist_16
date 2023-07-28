60290cf8bd210e822fa83f4286f7f4cc249e2c9e
Gaurav pandey <61mail2gaurav@gmail.com>
2023-07-28 10:01:07 +0530

    find maximun consective 1 in given array

---
+++ day_6/das2.js
@@ -0,0 +1,37 @@
+// find maximum  consecutive 1 in array 
+
+
+function myfun(  arr, n)
+{
+let count=0;
+let max=0;
+ for (let i=0;i<arr;i++)
+ {
+
+  if(arr[i]==1)
+  {
+
+  count++
+ let max=maths.max(max,count)
+
+  }
+
+  else{
+
+  count =0;
+
+
+  }
+
+
+  console.log(   max )
+
+
+
+
+ }
+
+
+
+
+}
