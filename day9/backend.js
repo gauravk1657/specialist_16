149870892a92b328f868154bcb7391eedb82454f
Gaurav pandey <61mail2gaurav@gmail.com>
2023-08-02 20:17:37 +0530

    backend applications

--- day9/backend.js
+++ day9/backend.js
@@ -1,1 +1,19 @@
 
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+const app = express();
+const upload = multer({ dest: 'uploads/' });
+
+app.use(cors()); 
+
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  
+  res.json({ message: 'File uploaded successfully!' });
+});
+
+
+app.listen(3001, () => {
+  console.log('Server running on port 3001');
+});
