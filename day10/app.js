4a03daa985162a7dd821c88a67b54b197f150011
Gaurav pandey <61mail2gaurav@gmail.com>
2023-08-04 22:35:44 +0530

    done

---
+++ day10/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ProductForm from "./components/ProductForm";
+import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <div className="App">
+      <ProductForm />
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
